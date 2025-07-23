e81be943e74e8e90d4dc7bc40f7e01b1541dab53
crazycoder46 <crazyprogrammer46@gmail.com>
2025-07-24 00:48:40 +0300

    Disable static rendering completely - simplify metro and app config

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -2,4 +2,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Disable static rendering for web builds
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+
+// Disable static rendering
+config.resolver = {
+  ...config.resolver,
+  platforms: ['ios', 'android', 'web'],
+};
+
 module.exports = config; 
